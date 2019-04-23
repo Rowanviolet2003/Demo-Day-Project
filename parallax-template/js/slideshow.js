d73ad3f61bb0491590ef92b74ef6c2713e77adab
Mission Bit <missionbit@154p-MissionBit.local>
2019-04-23 16:56:16 -0700

    added slideshow

---
+++ parallax-template/js/slideshow.js
@@ -0,0 +1,15 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instance = M.Carousel.init(elems);
+
+    instance.next(1);
+    instance.prev(1);
+});
+
+
+
+
+
+  
+
