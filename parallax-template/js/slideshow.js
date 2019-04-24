7086bc86aac90f7c64924731b188eac351b4da46
Mission Bit <missionbit@154p-MissionBit.local>
2019-04-23 17:58:15 -0700

    added bob ross

--- parallax-template/js/slideshow.js
+++ parallax-template/js/slideshow.js
@@ -2,6 +2,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
     var instance = M.Carousel.init(elems);
+    var instance = M.Carousel.init({
+        fullWidth: true,
+        indicators: true
+      });
+    
 
     instance.next(1);
     instance.prev(1);
